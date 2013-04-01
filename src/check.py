c773f506e25f43ac803dac2c37228faa0c97c6ce
Alastair Lynn <arplynn@gmail.com>
2013-04-01 10:58:42 +0100

    Make check messages print to stderr

--- src/check.py
+++ src/check.py
@@ -1,11 +1,12 @@
 import match_stats
+import sys
 
 
 def schedule_check(matches):
-    print '{0} matches total'.format(len(matches))
+    print >>sys.stderr, '{0} matches total'.format(len(matches))
     match_counter, opponents, collisions = match_stats.match_statistics(matches)
     all_teams = set(match_counter.iterkeys())
     for team, matches in match_counter.iteritems():
-        print '{0}: {1} matches, missed opponents: {2}'.format(team, matches, ', '.join(all_teams - opponents[team] - {team}))
+        print >>sys.stderr, '{0}: {1} matches, missed opponents: {2}'.format(team, matches, ', '.join(all_teams - opponents[team] - {team}))
         if team in collisions:
-            print '\t{0} DO have a match collision'.format(team)
+            print >>sys.stderr, '\t{0} DO have a match collision'.format(team)
