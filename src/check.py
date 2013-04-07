25c5ed2f5501b4944d9a270100feb14234eb7661
Alastair Lynn <arplynn@gmail.com>
2013-04-07 16:50:56 +0100

    Ignore surrogate appearances in checks

--- src/check.py
+++ src/check.py
@@ -1,12 +1,15 @@
 import match_stats
 import sys
+import utils
 
 
 def schedule_check(matches):
     print >>sys.stderr, '{0} matches total'.format(len(matches))
     match_counter, opponents, collisions = match_stats.match_statistics(matches)
-    all_teams = set(match_counter.iterkeys())
+    all_teams = set(x for x in match_counter.iterkeys() if utils.is_actual_entrant(x))
     for team, matches in match_counter.iteritems():
+        if not utils.is_actual_entrant(team):
+            continue
         print >>sys.stderr, '{0}: {1} matches, missed opponents: {2}'.format(team, matches, ', '.join(all_teams - opponents[team] - {team}))
         if team in collisions:
             print >>sys.stderr, '\t{0} DO have a match collision'.format(team)
