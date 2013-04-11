0fd23a1e23e0a97540fd0b10789e157d1fa4002b
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-04-11 15:27:50 +0100

    Use match2time to generate match times.

--- generate_matches.sh
+++ generate_matches.sh
@@ -14,14 +14,8 @@ for line in `./run full ${cwd}/teams-2013 102`; do
   name="match-${matchcount}"
   echo $name
 
-  # Starting at 13:20...
-  matchsub1=`expr $matchcount - 1`
-  # 5 minutes matches..
-  mins=`expr $matchsub1 \* 5`
-  mins=`expr $mins + 20`
-  hours=`expr $mins / 60`
-  mins=`expr $mins % 60`
-  hours=`expr $hours + 13`
+  matchtime=`${cwd}/match2time.py ${matchcount}`
+  echo $matchtime
 
   # Hard coded because bah.
   #/srv/compd/command add-match $name $time
