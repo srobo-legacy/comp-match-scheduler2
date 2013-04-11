ea161746a20ff47fa3199e02892c85eb66be3425
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-04-11 15:27:22 +0100

    Match scheduler contains an off-by-one

--- generate_matches.sh
+++ generate_matches.sh
@@ -3,7 +3,8 @@
 # Imported data; and Alistair assures me there are 103 matches this year...
 cwd=`pwd`
 matchcount=1
-for line in `./run full ${cwd}/teams-2013 103`; do
+# Actually say 102 matches, because this script always produces one more...
+for line in `./run full ${cwd}/teams-2013 102`; do
   # Fixme
   cd /srv/compd/command
 
