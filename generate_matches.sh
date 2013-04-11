25740fc8d220dcd5f4e0d2e817231c4955e7c2a6
Peter Law <PeterJCLaw@gmail.com>
2013-04-11 15:53:35 +0100

    path munging

--- generate_matches.sh
+++ generate_matches.sh
@@ -2,11 +2,12 @@
 
 # Imported data; and Alistair assures me there are 103 matches this year...
 cwd=`pwd`
+compd_dir=/srv/compd
 matchcount=1
 # Actually say 102 matches, because this script always produces one more...
 for line in `./run full ${cwd}/teams-2013 102`; do
   # Fixme
-  cd /srv/compd/
+  cd $compd_dir
 
   teamlist=`echo $line | tr '|' ' '`
 
@@ -18,7 +19,7 @@ for line in `./run full ${cwd}/teams-2013 102`; do
   echo $matchtime
 
   # Hard coded because bah.
-  /srv/compd/command add-match $name $matchtime
+  ./command add-match $name $matchtime
 
   matchcount=`expr $matchcount + 1`
 done
