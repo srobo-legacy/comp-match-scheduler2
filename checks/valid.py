3063c5747a4cc78f6002433df94e5b83d3ad65ac
Peter Law <PeterJCLaw@gmail.com>
2013-04-12 20:10:20 +0100

    Check for all teams scheduled.
    Change-Id: I32ebdc95efd5ddf16654b24c39c1720e74d55bb9

--- checks/valid.py
+++ checks/valid.py
@@ -1,10 +1,23 @@
 
 import sys
 
+all_teams = [x.strip() for x in open('teams-2013', 'r').readlines()]
+
 lines = open(sys.argv[1], 'r').readlines()
 
+found_teams = []
+
 for line in lines:
-    teams = line.strip().split('|')
+    teams = [x.strip() for x in line.strip().split('|')]
     assert len(set(teams)) == len(teams) == 4
+    found_teams += teams
+
+# Sanity
+assert len(set(all_teams)) == len(all_teams)
+
+# Sanity
+missing_teams = set(all_teams) - set(found_teams)
+print ', '.join(missing_teams)
+assert len(set(found_teams)) == len(all_teams)
 
 print 'Is valid'
