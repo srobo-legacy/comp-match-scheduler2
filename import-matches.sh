d502d0598c3e698daaa5d981d9e059fb2cda656b
Peter Law <PeterJCLaw@gmail.com>
2013-04-13 09:09:43 +0100

    Add a script to import the pre-generated schedule.
    Change-Id: I66dbcb2569cced9dd2c44e2509e3c25864309e9e

---
+++ import-matches.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Imported data; and Alistair assures me there are 103 matches this year...
+cwd=`pwd`
+compd_dir=/srv/compd
+matchcount=1
+# Actually say 102 matches, because this script always produces one more...
+for line in `cat out`; do
+  # Fixme
+  cd $compd_dir
+
+  teamlist=`echo $line | tr '|' ' '`
+
+  # Match name
+  name="match-${matchcount}"
+  echo $name
+
+  matchtime=`${cwd}/match2time.py ${matchcount}`
+  echo $matchtime
+
+  # Hard coded because bah.
+  ./command add-match $name $matchtime
+  ./command set-match-teams $name $teamlist
+
+  matchcount=`expr $matchcount + 1`
+done
