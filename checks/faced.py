5416b2766173c98e875515edc836108375744859
Peter Law <PeterJCLaw@gmail.com>
2013-04-20 12:40:36 +0100

    Generate far more data about teams' opponents.
    Change-Id: Idf1ed4bd665e1dc3b31049c3cb3adbf752363950

--- checks/faced.py
+++ checks/faced.py
@@ -2,6 +2,8 @@
 import collections
 import sys
 
+VERBOSE = False
+
 if len(sys.argv) != 2 or '--help' in sys.argv:
     print 'Usage: faced.py <schedule-file>'
     print '  Displays statistics about which others a team have faced'
@@ -19,7 +21,32 @@ for match in matches:
         for faces in match:
             c[tla][faces] += 1
 
-for tla, opponents in c.iteritems():
-    opponents = opponents.keys()
-    print '{0} faces {1} opponents: {2}'.format(tla, len(opponents), opponents)
+all_teams = set(c.keys())
 
+# total appearances / teams => max appearances per team
+# 4.0 is teams-per-match
+matches_per_team = int(round(len(matches) * 4.0 / len(all_teams)))
+
+# 4.0 means this is 1/4 of a team's matches
+LOTS_REPEATS_LIMIT = int(round(matches_per_team / 4.0))
+
+for tla, opponents in c.iteritems():
+    missed = all_teams - set(opponents.keys())
+    del opponents[tla]
+    all_repeats = {}
+    lots_repeats = {}
+    faced = opponents.keys()
+    for opp in faced:
+        times = opponents[opp]
+        if times > 1:
+            all_repeats[opp] = times
+        if times > LOTS_REPEATS_LIMIT:
+            lots_repeats[opp] = times
+    if VERBOSE:
+        print '{0} faces {1} opponents: {2}'.format(tla, len(faced), faced)
+        print '{0} repeats {1} opponents: {2}'.format(tla, len(all_repeats), all_repeats)
+        print '{0} repeats {1} opponents lots of times: {2}'.format(tla, len(lots_repeats), lots_repeats)
+        print '{0} misses {1} opponents: {2}'.format(tla, len(missed), missed)
+    else:
+        print '{0: <4} faces {1: >2}, misses {2: >2}, repeats {3: >2} more than {4} times.' \
+                .format(tla, len(faced), len(missed), len(lots_repeats), LOTS_REPEATS_LIMIT)
